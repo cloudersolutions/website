e44fb8d5f698b182f3ef614c558162305a1a4bbd
Mathias Lindholm <mathias.lindholm@cloudersolutions.com>
2025-03-06 10:24:17 +0200

    Cleanup

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,12 @@
 /// <reference types="astro/client" />
 
-declare module '*.yml' {
-  const value: any;
+declare module '@data/navigation.yml' {
+  import type { NavItem } from '@data/types';
+  const value: MenuItem[];
+  export default value;
+}
+
+declare module '@data/metadata.yml' {
+  const value: Record<string, string>;
   export default value;
 }
