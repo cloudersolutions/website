27a14d54d13201e0bf9800bfae42e820fb5c8c29
Mathias Lindholm <mathias.lindholm@cloudersolutions.com>
2025-03-07 14:26:37 +0200

    Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ['./src/**/*.{astro,md,ts}'],
   theme: {
-    extend: {}
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.5rem'
+      }
+    },
+    extend: {
+      typography: () => ({
+        neutral: {
+          // this applies to .prose class
+          css: {
+            'h1, h2, h3, h4, h5, h6': {
+              fontWeight: '600'
+            },
+            strong: {
+              fontWeight: '600'
+            }
+          }
+        }
+      })
+    }
   },
-  plugins: []
+  plugins: [require('@tailwindcss/typography')]
 };
