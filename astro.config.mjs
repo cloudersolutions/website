5987685c7bce0501c3f0729b67444ef96017cd91
Mathias Lindholm <mathias.lindholm@cloudersolutions.com>
2025-03-12 10:57:19 +0200

    Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  //TODO: change to actual url
+  site: 'https://clouderinuudetkottarit.vercel.app',
   vite: {
     plugins: [yaml(), tailwindcss()]
   },
