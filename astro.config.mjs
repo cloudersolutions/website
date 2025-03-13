58f483e1700f03aa9c16e1842b37b7ab259ea5c6
Mathias Lindholm <mathias.lindholm@cloudersolutions.com>
2025-03-13 17:58:27 +0200

    Prefetching and transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: {
+    prefetchAll: true
+  },
   site: process.env.URL,
   vite: {
     plugins: [yaml(), tailwindcss()]
