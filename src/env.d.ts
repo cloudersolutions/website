8eb5d9fc769711181ea9c969fc6d7f228954a7db
Mathias Lindholm <mathias.lindholm@cloudersolutions.com>
2025-03-14 11:35:59 +0200

    CMS

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,11 +2,15 @@
 
 declare module '@data/navigation.yml' {
   import type { NavItem } from '@data/types';
-  const value: MenuItem[];
+  interface NavData {
+    items: NavItem[];
+  }
+  const value: NavData;
   export default value;
 }
 
 declare module '@data/site.yml' {
+  import type { NavItem } from '@data/types';
   interface SiteData {
     [key: string]: string;
     social: NavItem[];
