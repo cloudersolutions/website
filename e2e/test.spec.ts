7ffca09feda496155735d363e4f13f15bbf4a99d
Mathias Lindholm <mathias.lindholm@cloudersolutions.com>
2025-09-08 23:44:08 +0300

    Refactor layout and styles for improved consistency

--- e2e/test.spec.ts
+++ e2e/test.spec.ts
@@ -5,12 +5,16 @@ test('render home page with key sections', async ({ page }) => {
   await expect(page).toHaveTitle('Clouder');
   await expect(page.locator('nav')).toBeVisible();
   await expect(
-    page.getByRole('heading', { name: 'The market-leading data and integration platform' })
+    page.getByRole('heading', { name: 'Turn building performance into value' })
   ).toBeVisible();
-  await expect(page.getByTestId('overline').getByText('Benefits')).toBeVisible();
-  await expect(page.getByTestId('overline').getByText('Solution')).toBeVisible();
-  await expect(page.getByTestId('overline').getByText('Product')).toBeVisible();
-  await expect(page.getByTestId('overline').getByText('Sustainability')).toBeVisible();
+  await expect(
+    page.getByRole('heading').getByText('The future of real estate is data')
+  ).toBeVisible();
+  await expect(page.getByRole('heading').getByText('Increased asset value')).toBeVisible();
+  await expect(page.getByRole('heading').getByText('Get up and running in 14 days')).toBeVisible();
+  await expect(
+    page.getByRole('heading').getByText('Learn how our customers are using Clouder')
+  ).toBeVisible();
 });
 
 test('product page navigation', async ({ page }) => {
