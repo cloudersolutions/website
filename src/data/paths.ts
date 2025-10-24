c35b58c02f4304f8b1f840b5a5c81741f600482e
Mathias Lindholm <mathias.lindholm@cloudersolutions.com>
2025-10-24 23:35:50 +0300

    Refactor navigation data files
    - Replace navigation.yml with paths.ts for all navigation links
    - Move site config to config.yml and update imports
    - Remove social links from CMS

---
+++ src/data/paths.ts
@@ -0,0 +1,59 @@
+export const paths = {
+  home: {
+    title: 'Home',
+    href: '/'
+  },
+  product: {
+    title: 'Product',
+    href: '/product'
+  },
+  news: {
+    title: 'News',
+    href: '/news'
+  },
+  about: {
+    title: 'About',
+    href: '/about'
+  },
+  contact: {
+    title: 'Contact',
+    href: '/contact'
+  },
+  privacy: {
+    title: 'Privacy policy',
+    href: '/privacy-policy'
+  },
+  infosec: {
+    title: 'Information security',
+    href: '/information-security-practices'
+  },
+  dashboard: {
+    title: 'Log in',
+    href: 'https://dashboard.cloudersolutions.com/'
+  }
+};
+
+export const headerLinks = [paths.product, paths.news, paths.about, paths.contact, paths.dashboard];
+
+export const sitemapLinks = [paths.home, paths.product, paths.news, paths.about, paths.contact];
+
+export const resourcesLinks = [paths.privacy, paths.infosec, paths.dashboard];
+
+export const socialLinks = [
+  {
+    title: 'LinkedIn',
+    href: 'https://www.linkedin.com/company/clouder/about/'
+  },
+  {
+    title: 'X (Twitter)',
+    href: 'https://x.com/Clouder_tech'
+  },
+  {
+    title: 'Instagram',
+    href: 'http://instagram.com/cloudersolutions'
+  },
+  {
+    title: 'YouTube',
+    href: 'https://www.youtube.com/@cloudersolutions'
+  }
+];
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,19 +1,8 @@
 /// <reference types="astro/client" />
 
-interface NavItem {
-  title: string;
-  href: string;
-}
-
-declare module '@data/navigation.yml' {
-  const value: Record<string, NavItem>;
-  export default value;
-}
-
-declare module '@data/site.yml' {
+declare module '@data/config.yml' {
   interface SiteData {
     [key: string]: string;
-    social: NavItem[];
   }
   const value: SiteData;
   export default value;
