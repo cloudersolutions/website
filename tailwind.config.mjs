4fd169b967a7c0c546b89fbea977fc7a8cf5e305
Mathias Lindholm <mathias.lindholm@cloudersolutions.com>
2026-02-18 22:26:59 +0200

    Add scrolling logo row

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -112,7 +112,16 @@ export default {
             '--tw-prose-links-hover': 'var(--tw-prose-invert-links-hover)'
           }
         }
-      })
+      }),
+      animation: {
+        'loop-scroll': 'loop-scroll 50s linear infinite'
+      },
+      keyframes: {
+        'loop-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' }
+        }
+      }
     }
   },
   plugins: [typography, forms]
