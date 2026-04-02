bf6b20a84b4f435936ebb43de5a79c51b636082c
Mathias Lindholm <mathias.lindholm@cloudersolutions.com>
2026-04-02 16:00:54 +0300

    [WIP] Change brand fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,16 +12,23 @@ export default {
             lineHeight: '1.625',
             maxWidth: '70ch',
             blockquote: {
-              marginTop: '2rem',
-              marginBottom: '2rem',
               border: 'none',
-              padding: 0,
-              lineHeight: '1.5'
+              fontStyle: 'normal',
+              fontWeight: '400',
+              marginBottom: '2rem',
+              marginTop: '2rem',
+              padding: 0
             },
             'blockquote p:first-of-type': {
+              fontFamily: 'var(--font-serif)',
               fontSize: '1.25rem',
-              fontWeight: '500',
-              textIndent: '-0.45em'
+              lineHeight: '1.75rem',
+              textIndent: '-0.45em',
+              margin: 0,
+              '@media (width >= 64rem)': {
+                fontSize: '1.5rem',
+                lineHeight: '2rem'
+              }
             },
             'blockquote p:first-of-type::before': {
               content: 'open-quote'
@@ -34,10 +41,8 @@ export default {
             },
             'blockquote p:last-of-type:not(p:only-child)': {
               color: 'var(--tw-prose-captions)',
-              fontStyle: 'normal',
               fontSize: '0.875rem',
-              fontWeight: '400',
-              marginTop: '-0.75rem',
+              marginTop: '0.75rem',
               marginBottom: 0
             },
             'blockquote p:last-of-type:not(p:only-child)::before': {
@@ -53,8 +58,8 @@ export default {
               marginBlock: '2rem'
             },
             li: {
-              marginTop: '.375em',
-              marginBottom: '.375em'
+              marginTop: '0.25em',
+              marginBottom: '0.25em'
             },
             // Fix for CMS/TipTap/ProseMirror markdown lists. ProseMirror wraps list item content in a paragrap tag, which messes up spacing.
             'li > p': {
@@ -65,36 +70,48 @@ export default {
         },
         neutral: {
           css: {
-            '--tw-prose-body': 'var(--color-neutral-700)',
-            '--tw-prose-bullets': 'var(--color-neutral-700)',
-            '--tw-prose-counters': 'var(--color-neutral-700)',
-            '--tw-prose-bold': 'var(--color-neutral-800)',
+            '--tw-prose-body': 'var(--color-neutral-900)',
+            '--tw-prose-bullets': 'var(--color-neutral-900)',
+            '--tw-prose-counters': 'var(--color-neutral-900)',
+            '--tw-prose-bold': 'var(--color-neutral-900)',
             '--tw-prose-headings': 'var(--color-neutral-950)',
             '--tw-prose-quotes': 'var(--color-neutral-950)',
             '--tw-prose-captions': 'var(--color-neutral-500)',
-            '--tw-prose-links': 'var(--color-indigo-500)',
-            '--tw-prose-links-hover': 'var(--color-indigo-700)',
-            '--tw-prose-invert-body': 'var(--color-neutral-300)',
-            '--tw-prose-invert-bullets': 'var(--color-neutral-300)',
-            '--tw-prose-invert-counters': 'var(--color-neutral-300)',
-            '--tw-prose-invert-bold': 'var(--color-neutral-200)',
+            '--tw-prose-links': 'royalblue',
+            '--tw-prose-links-hover': 'var(--color-blue-700)',
+            '--tw-prose-invert-body': 'var(--color-neutral-100)',
+            '--tw-prose-invert-bullets': 'var(--color-neutral-100)',
+            '--tw-prose-invert-counters': 'var(--color-neutral-100)',
+            '--tw-prose-invert-bold': 'var(--color-neutral-50)',
             '--tw-prose-invert-quotes': 'var(--color-neutral-50)',
             '--tw-prose-invert-headings': 'var(--color-neutral-50)',
             '--tw-prose-invert-captions': 'var(--color-neutral-500)',
-            '--tw-prose-invert-links': 'var(--color-indigo-400)',
-            '--tw-prose-invert-links-hover': 'var(--color-indigo-300)',
+            '--tw-prose-invert-links': 'var(--color-blue-400)',
+            '--tw-prose-invert-links-hover': 'var(--color-blue-300)',
             'h1, h2, h3, h4, h5, h6': {
-              fontWeight: '500'
+              fontWeight: '500',
+              marginBottom: '1rem'
             },
             h1: {
-              marginBottom: '0.666667em'
+              fontSize: '2rem',
+              lineHeight: '1.166665'
             },
             h2: {
-              marginTop: '1.8em',
-              marginBottom: '0.8em'
+              fontSize: '1.5rem',
+              lineHeight: '1.33334',
+              marginTop: '3rem'
+            },
+            h3: {
+              fontSize: '1.125rem',
+              lineHeight: '1.55556',
+              marginTop: '2rem'
+            },
+            h4: {
+              fontSize: '1rem',
+              lineHeight: '1.625'
             },
             strong: {
-              fontWeight: '600'
+              fontWeight: '550'
             },
             a: {
               fontWeight: '400',
