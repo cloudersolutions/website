df5e68fb5dcbce72a448d36025d3bdbc393c8c26
Mathias Herler <mathias.lindholm@cloudersolutions.com>
2026-06-02 22:47:11 +0300

    Tweak chart

--- src/assets/performanceData.ts
+++ src/assets/performanceData.ts
@@ -1,5 +1,6 @@
 export const data = {
   prev: {
+    year: new Date().getFullYear() - 1,
     budgetMwh: '1,300 MWh',
     adjustedMwh: '1,215 MWh',
     adjustedMwhDesc: '6.5% below budget due to lower heating need',
@@ -7,8 +8,8 @@ export const data = {
     actualMwh: '1,756 MWh',
     actualMwhDesc: '44.5% above adjusted target',
     valuePotTitle: 'Value potential',
-    valuePot: '€1,082,000',
-    valuePotDesc: 'Consumption 541 MWh above adjusted target. NOI leakage €54,100.',
+    valuePot: '€901,667',
+    valuePotDesc: 'NOI leakage €54,100',
     kwhPsm: '130 kWh/m²',
     kgCo2Psm: '23kg CO₂e/m²',
     actual: [
@@ -18,6 +19,7 @@ export const data = {
     estimate: Array(13).fill(null)
   },
   curr: {
+    year: new Date().getFullYear(),
     budgetMwh: '1,300 MWh',
     adjustedMwh: '1,290 MWh',
     adjustedMwhDesc: '0.8% below budget due to lower heating need',
@@ -25,8 +27,8 @@ export const data = {
     actualMwh: '1,275 MWh',
     actualMwhDesc: '1.2% below adjusted target',
     valuePotTitle: 'Value created',
-    valuePot: '€30,000',
-    valuePotDesc: 'Consumption 15 MWh below adjusted target. NOI increased by €1,500.',
+    valuePot: '€25,000',
+    valuePotDesc: 'NOI increased by €1,500',
     kwhPsm: '95 kWh/m²',
     kgCo2Psm: '17kg CO₂e/m²',
     actual: [0, 10.6, 20.6, 28.6, 36.4, 44.2],
