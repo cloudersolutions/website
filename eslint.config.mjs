bf4fcdb041932d2166ff1e90b1c310ace4dd3b3e
Mathias Herler <mathias.lindholm@cloudersolutions.com>
2026-07-02 14:19:07 +0300

    Update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import eslint from '@eslint/js';
 import tsEslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import astroPlugin from 'eslint-plugin-astro';
-import astroParser from 'astro-eslint-parser';
+import * as astroParser from 'astro-eslint-parser';
 
 export default [
   {
